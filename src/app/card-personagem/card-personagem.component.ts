1e1e3f56687e59e8c3745cbc88317ec6a0a754e1
Pdutra <powdutra26@gmail.com>
2022-12-08 13:13:18 -0300

    Exercicio 5

--- src/app/card-personagem/card-personagem.component.ts
+++ src/app/card-personagem/card-personagem.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class CardPersonagemComponent {
 
+  personagem: any = {
+    imagem: 'https://upload.wikimedia.org/wikipedia/pt/thumb/6/6f/JonSnow_08.jpg/220px-JonSnow_08.jpg',
+    nome: 'Jon Snow (Game of Thrones)',
+    nomeAtor: 'Kit Harington',
+    descricao:
+      'Jon Snow é um personagem fictício da série de livros de fantasia A Song of Ice and Fire, escrita pelo autor norte-americano George R. R. Martin. Ele também é um dos personagens principais da adaptação televisiva da HBO, Game of Thrones, onde é interpretado pelo ator britânico Kit Harington.'
+  };
+
 }
