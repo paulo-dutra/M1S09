ac6328ee62adbdd3dbb3c6dd0a1b7f42590b0eb9
Pdutra <powdutra26@gmail.com>
2022-12-08 01:56:06 -0300

    Exercicio 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
